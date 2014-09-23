0ee0f78393289be7af49e9d0edd2fdea102b5b0a
brian <bboyd019@yahoo.com>
2014-09-22 21:08:12 -0500

    initial functionality for github ajax request

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,26 @@
 (function () {
 	'use strict';
-
 	
+
+	function getRepos() {
+		var request = new XMLHttpRequest(),
+			url = 'https://api.github.com/users/briankboyd/repos';
+//		request.onload = function () {
+//			console.log(request.responseText);
+//		}
+		function getRepoText() {
+			request.onreadystatechange = function () {
+				if (request.readyState === 4) {
+					return request.responseText;
+				}
+			};
+		}
+		request.open('GET', url, false);
+		request.send();
+		
+		return getRepoText;
+	}
+	
+	console.log(getRepos());
 }());
 
